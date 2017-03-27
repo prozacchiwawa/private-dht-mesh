678af4f9b73d737141bb3eebaa2a3306f5b6748e
prozacchiwawa <art.yerkes@gmail.com>
2017-03-27 15:23:51 -0700

    Add test

--- test/kbuckettest.fs
+++ test/kbuckettest.fs
@@ -430,4 +430,113 @@ let tests =
         let _ = kb := kb2 in
         massert.ok (not (highBucketMatch toDelete)) ;
         donef ()
+
+  ; "adding a contact does not split K-bucket" =>
+      fun donef ->
+        let kb = ref (KBucket.init (nodeId (ShortId.generate ()))) in
+        let pings = ref [] in
+        kbadd kb pings (newContactString "a") ;
+        (match ((!kb).bucket,(!kb).low,(!kb).high) with
+         | (Some bucket,None,None) -> massert.ok true
+         | _ -> massert.ok false
+        ) ;
+        donef ()
+
+  ; (String.concat " "
+       ["adding maximum number of contacts (per K-bucket) [" ;
+        string KBucket.Constants.DEFAULT_NUMBER_OF_NODES_PER_K_BUCKET ;
+        "]" ;
+        " into K-bucket does not split K-bucket"
+       ]
+    ) =>
+      fun donef ->
+        let kb = ref (KBucket.init (nodeId (ShortId.generate ()))) in
+        let pings = ref [] in
+        for i = 0 to KBucket.Constants.DEFAULT_NUMBER_OF_NODES_PER_K_BUCKET - 1 do
+          begin
+            kbadd kb pings (newContactString (string i))
+          end ;
+        (match ((!kb).bucket,(!kb).low,(!kb).high) with
+         | (Some bucket,None,None) -> massert.ok true
+         | _ -> massert.ok false
+        ) ;
+        donef ()
+
+(*
+test['adding maximum number of contacts (per K-bucket) + 1 [' +
+     (constants.DEFAULT_NUMBER_OF_NODES_PER_K_BUCKET + 1) + ']' +
+     ' into K-bucket splits the K-bucket'] = function (test) {
+    test.expect(3);
+    var kBucket = new KBucket();
+    for (var i = 0; i < constants.DEFAULT_NUMBER_OF_NODES_PER_K_BUCKET + 1; i++) {
+        kBucket.add({id: new Buffer("" + i)});
+          }
+    test.ok(kBucket.low instanceof KBucket);
+    test.ok(kBucket.high instanceof KBucket);
+    test.ok(!kBucket.bucket);
+    test.done();
+                                                        };
+
+test['split buckets contain all added contacts'] = function (test) {
+    test.expect(constants.DEFAULT_NUMBER_OF_NODES_PER_K_BUCKET + 2);
+    var kBucket = new KBucket({localNodeId: new Buffer('00', 'hex')});
+    var foundContact = {};
+    for (var i = 0; i < constants.DEFAULT_NUMBER_OF_NODES_PER_K_BUCKET + 1; i++) {
+        var iString = i.toString('16');
+        if (iString.length < 2) {
+            iString = '0' + iString;
+             }
+        kBucket.add({id: new Buffer(iString, 'hex')});
+        foundContact[iString] = false;
+          }
+    var traverse = function (node) {
+        if (!node.bucket) {
+            traverse(node.low);
+            traverse(node.high);
+             } else {
+            node.bucket.forEach(function (contact) {
+                foundContact[contact.id.toString('hex')] = true;
+                                           });
+          }
+                              };
+    traverse(kBucket);
+    Object.keys(foundContact).forEach(function (key) {
+        test.ok(foundContact[key], key);
+                                                 });
+    test.ok(!kBucket.bucket);
+    test.done();
+                                                              };
+
+test['when splitting buckets the "far away" bucket should be marked' +
+     ' to prevent splitting "far away" bucket'] = function (test) {
+    test.expect(5);
+    var kBucket = new KBucket({localNodeId: new Buffer('00', 'hex')});
+    for (var i = 0; i < constants.DEFAULT_NUMBER_OF_NODES_PER_K_BUCKET + 1; i++) {
+        var iString = i.toString('16');
+        if (iString.length < 2) {
+            iString = '0' + iString;
+             }
+        kBucket.add({id: new Buffer(iString, 'hex')});
+          }
+    // above algorithm will split low bucket 4 times and put 0x00 through 0x0f
+    // in the low bucket, and put 0x10 through 0x14 in high bucket
+    // since localNodeId is 0x00, we expect every high bucket to be "far" and
+    // therefore marked as "dontSplit = true"
+    // there will be one "low" bucket and four "high" buckets (test.expect(5))
+    var traverse = function (node, dontSplit) {
+        if (!node.bucket) {
+            traverse(node.low, false);
+            traverse(node.high, true);
+             } else {
+            if (dontSplit) {
+                test.ok(node.dontSplit);
+                 } else {
+                test.ok(!node.dontSplit);
+              }
+          }
+                              };
+    traverse(kBucket);
+    test.done();
+                                                             };
+*)
   ]
